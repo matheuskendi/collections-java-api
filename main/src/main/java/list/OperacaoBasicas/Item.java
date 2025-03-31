4f61654f141d16edab3fbc2550676f2862b881d3
kendi <matheus.mks23@gmail.com>
2025-03-31 12:51:41 -0300

    operação basica list

---
+++ main/src/main/java/list/OperacaoBasicas/Item.java
@@ -0,0 +1,4 @@
+package main.java.list.OperacaoBasicas;
+
+public class Item {
+}
---
+++ main/src/main/java/list/OperacaoBasicas/Tarefa.java
@@ -0,0 +1,16 @@
+package main.java.list.OperacaoBasicas;
+
+public class Tarefa {
+
+    //Atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
